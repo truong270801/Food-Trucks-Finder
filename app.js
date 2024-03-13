edba92ad44d8c6384cde1d71716a3e47a5fffa57
truong270801 <tranvantruong27082001@gmail.com>
2024-03-14 01:45:45 +0700

    update

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiYmV0YXBjaG9pMTBrIiwiYSI6ImNrY2ZuaWEwNjA2ZW0yeWw4bG9yNnUyYm0ifQ.bFCQ-5yq6cSsrhugfxO2_Q';
 const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v11', // style URL
-    center: [-122.4309147,37.7331262], // starting position [lng, lat]
-    zoom: 5, // starting zoom
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [-122.4788854, 37.7577607,], 
+    zoom: 9,
     hash: 'map'
 });
 
-
-
+//API
 async function FoodTruckFinder() {
   try {
     let apiURL = 'https://data.sfgov.org/resource/rqzj-sfat.json';
@@ -21,8 +20,11 @@ async function FoodTruckFinder() {
           coordinates: [parseFloat(truck.longitude), parseFloat(truck.latitude)]
         },
         properties: {
-          name: truck.applicant,
-          fooditems: truck.fooditems
+          applicant: truck.applicant,
+          fooditems: truck.fooditems,
+          facilitytype: truck.facilitytype ,
+
+
         }
       };
     });
@@ -40,17 +42,17 @@ async function FoodTruckFinder() {
     // Load biểu tượng tùy chỉnh
     map.loadImage('img/location.png', function(error, image) {
       if (error) throw error;
-      map.addImage('custom-icon', image); // Thêm biểu tượng tùy chỉnh vào bản đồ
+      map.addImage('custom-icon', image); 
     });
 
     map.addLayer({
       id: 'food-truck-points',
-      type: 'symbol', // Thay đổi loại layer từ 'circle' sang 'symbol'
+      type: 'symbol', 
       source: 'food-truck-source',
       layout: {
-        'icon-image': 'custom-icon', // Sử dụng biểu tượng tùy chỉnh
-        'icon-size': 0.75, // Kích thước của biểu tượng
-        'icon-allow-overlap': true // Cho phép biểu tượng chồng lên nhau
+        'icon-image': 'custom-icon', 
+        'icon-size': 0.75, 
+        'icon-allow-overlap': true 
       }
     });
     map.addLayer({
@@ -71,28 +73,27 @@ async function FoodTruckFinder() {
     console.error('Error fetching data:', error);
   }
 }
-
 FoodTruckFinder();
 
-// Thêm xử lý sự kiện click vào biểu tượng xe ăn
+// Thêm xử lý sự kiện click vào biểu tượng
 map.on('click', 'food-truck-points', function (e) {
   var coordinates = e.features[0].geometry.coordinates.slice();
-  var name = e.features[0].properties.name;
+  var name = e.features[0].properties.applicant;
   var foodItems = e.features[0].properties.fooditems;
+  var facilityType = e.features[0].properties.facilitytype;
 
   // Hiển thị thông tin trong một popup
   new mapboxgl.Popup()
     .setLngLat(coordinates)
-    .setHTML('<h3>' + name + '</h3><p>' + foodItems + '</p>')
+    .setHTML('<h3>' + name + '</h3><p><b>Facility Type: </b>' + facilityType + '</p><p><b>Food Items: </b>' + foodItems + '</p>')
     .addTo(map);
 });
-
-// Thay đổi con trỏ chuột khi di chuột qua các biểu tượng xe ăn
+// Thay đổi con trỏ chuột khi di chuột qua các biểu tượng
 map.on('mouseenter', 'food-truck-points', function () {
   map.getCanvas().style.cursor = 'pointer';
 });
 
-// Thay đổi lại con trỏ chuột khi di chuột ra khỏi các biểu tượng xe ăn
+// Thay đổi lại con trỏ chuột khi di chuột ra khỏi các biểu tượng
 map.on('mouseleave', 'food-truck-points', function () {
   map.getCanvas().style.cursor = '';
 });
